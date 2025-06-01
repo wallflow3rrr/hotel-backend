2cd2b952410c458b86b3249d0c92455e8c1ad29f
Legion <khokhryakov2004@gmail.com>
2025-06-01 21:30:42 +0400

    add refresh token

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'hotel_secret_key';
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'hotel_refresh_secret_key';
 
 export const authenticateJWT = (
   req: Request,
@@ -18,9 +19,28 @@ export const authenticateJWT = (
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     (req as any).admin = decoded;
-    next();
+    return next();
   } catch (err) {
-    console.error('Ошибка верификации токена:', err);
-    res.status(403).json({ error: 'Недействительный токен' });
+    return next();
+  }
+};
+
+export const checkRefreshToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const refreshToken = req.cookies?.refreshToken;
+
+  if (!refreshToken) {
+    res.status(401).json({ error: 'Refresh токен не найден' });
+  }
+
+  try {
+    const decoded = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET) as { id: number };
+    (req as any).adminId = decoded.id;
+    return next();
+  } catch (err) {
+    res.status(403).json({ error: 'Недействительный refresh токен' });
   }
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,12 @@
 
 import express from 'express';
 import { loginAdmin } from '../controllers/auth.controller';
+import { refreshAccessToken} from '../controllers/auth.controller';
+import {checkRefreshToken} from '../middleware/auth.middleware'
 
 const router = express.Router();
 
 router.post('/login', loginAdmin);
+router.post('/refresh-token', checkRefreshToken, refreshAccessToken);
 
 export default router;
